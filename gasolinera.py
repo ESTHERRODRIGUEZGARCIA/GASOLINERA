e9a7e6898465d8278bdc49827dfd52f815d58ccd
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2023-04-17 17:11:22 +0200

    cerrado

--- gasolinera.py
+++ gasolinera.py
@@ -2,7 +2,7 @@
 #hay n surtidores
 #se generan 50 coches que son threads
 #a la gasolinera llgan coches de forma aleatoria de hasta 15 minutos
-#cuando un coche se pone en el surtidor, se baja el conductor, elije el combistible y elige la cantidad y llena el depósito entre 5 y 10 minutos
+#cuando un coche se pone en el surtidor, se baja el conductor, elige el combistible y elige la cantidad y llena el depósito entre 5 y 10 minutos
 #luego paga en una caja única y se va
 #en pagar se tarda 3 minutos
 #al irse, queda el surtidor libre
@@ -35,22 +35,26 @@ def coche():
             print("El conductor elige gasolina")
         else:
             print("El conductor elige diesel")
-        #se elige la cantidad
+        #el conductor llena el depósito
         cantidad = random.randint(5,10)
-        print("El conductor elige ", cantidad, " litros")
-        #se llena el depósito
+        print("El conductor elige una cantidad de", cantidad, "litros")
         print("El conductor llena el depósito")
-        time.sleep(5)
+        time.sleep(0.1)
         #se paga en la caja única
         print("El conductor paga en la caja única")
-        time.sleep(3)
+        time.sleep(0.1)
         #se va
         print("El conductor se va")
         coches -= 1
+    return coches
 
 def main():
     #se crea el thread
     t = threading.Thread(target=coche)
     #se inicia el thread
     t.start()
+    #se espera a que termine el thread
+    t.join()
 
+if __name__ == "__main__":
+    main()
